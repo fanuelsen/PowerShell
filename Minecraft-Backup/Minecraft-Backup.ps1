d5bc1a2c3c41f7c60c2d48bfa9e28a3c257b343a
fanuelsen@mac.com <fanuelsen@mac.com>
2020-02-06 12:06:56 +0100

    Made script more dynamic

--- Minecraft-Backup/Minecraft-Backup.ps1
+++ Minecraft-Backup/Minecraft-Backup.ps1
@@ -1,13 +1,16 @@
-$DomainName = "YourDomainHere" #For cloud only, use AzureAD
+$DomainName = "AzureAD"
 $Username = Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty Username
 $Username = $Username.Replace("$DomainName\", "")
-
-$Source = Get-ChildItem "C:\Users\$Username\AppData\Local\Packages\Microsoft.MinecraftEducationEdition_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds"
+$Date = Get-Date -Format "dd-MM-yyyy"
+$MinecraftAppDir = Get-ChildItem C:\Users\$Username\AppData\Local\Packages\ -recurse | Where-Object { $_.Name -match "Microsoft.MinecraftEducation" }
+$MinecraftWorldDir = Get-ChildItem $MinecraftAppDir.FullName -recurse | Where-Object { $_.Name -match "minecraftWorlds" }
+$Source = Get-ChildItem $MinecraftWorldDir.Fullname
 $Target = "C:\Backup"
+
 If(!(Test-Path $Target)) {
     New-Item -ItemType Directory -Path $Target -Force
 }
 
 foreach ($Dir in $Source) {
-    Compress-Archive -Path $dir.fullname -DestinationPath "$Target\$Dir-$Date.zip"
+    Compress-Archive -Path $Dir.FullName -DestinationPath "$Target\$Dir-$Date.zip" -Force
 }
